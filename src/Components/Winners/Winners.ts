0b43c5551fb1fc3474c918ff60b1950806943409
max-makk <90equals50@gmail.com>
2022-08-03 08:07:59 -0700

    feat: add table sorting

--- src/Components/Winners/Winners.ts
+++ src/Components/Winners/Winners.ts
@@ -10,18 +10,40 @@ export default class Winners {
 
   page: number;
 
+  sort: 'wins' | 'time';
+
+  order: 'ASC' | 'DESC';
+
+  btnWins: HTMLElement;
+
+  btnTime: HTMLElement;
+
   constructor() {
     this.page = 1;
     this.count = '0';
     this.section = document.querySelector('.section-winners');
     this.tbody = document.querySelector('tbody');
+    this.sort = 'wins';
+    this.order = 'DESC';
+    this.btnWins = document.querySelector('.btn-wins');
+    this.btnTime = document.querySelector('.btn-time');
+    this.btnWins.addEventListener('click', () => {
+      this.sort = 'wins';
+      this.order = this.order === 'ASC' ? 'DESC' : 'ASC';
+      this.init();
+    });
+    this.btnTime.addEventListener('click', () => {
+      this.sort = 'time';
+      this.order = this.order === 'ASC' ? 'DESC' : 'ASC';
+      this.init();
+    });
   }
 
   async init() {
     this.section.style.display = 'block';
     this.tbody.textContent = '';
     const wins = await s.getWinners({
-      page: this.page, limit: 10, sort: 'wins', order: 'ASC',
+      page: this.page, limit: 10, sort: this.sort, order: this.order,
     });
     this.count = wins.count;
     wins.items.forEach((el) => {
