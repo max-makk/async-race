54edc00b32337ef69b9ef732e84b3b822a76608a
max-makk <90equals50@gmail.com>
2022-08-04 08:59:46 -0700

    feat: reduce font for table

--- src/Components/Winners/WinnersUI.ts
+++ src/Components/Winners/WinnersUI.ts
@@ -3,7 +3,7 @@ import { WinnerInfo } from '../../services/interfaces';
 const WinnersUI = (data: WinnerInfo) => (`
   <tr>
     <td>${data.id}</td>
-    <td style="color: ${data.car.color}">__/‾‾|‾‾\\___<br/>'–◯–—––◯–'</td>
+    <td style="color: ${data.car.color}; font-size: 12px">__/‾‾|‾‾\\___<br/>'–◯–—––◯–'</td>
     <td>${data.car.name}</td>
     <td>${data.wins}</td>
     <td>${data.time}</td>
